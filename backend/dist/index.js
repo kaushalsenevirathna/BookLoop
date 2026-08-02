c8d30c36ee28da9be12b240f511a0e87d3ae51e8
Kaushal Senevirathna <kaushalsenevirathnaofficial@gmail.com>
2026-08-02 18:00:26 +0530

    Stop tracking dist folder, ignore it going forward

--- backend/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// A simple console message to confirm TypeScript compiles and runs correctly
-console.log("Backend project is set up correctly!");
-//# sourceMappingURL=index.js.map
