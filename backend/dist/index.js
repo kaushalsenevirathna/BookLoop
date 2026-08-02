32a195d0d4be2cd961ae7574c1ba910f3d7e7a32
Kaushal Senevirathna <kaushalsenevirathnaofficial@gmail.com>
2026-08-02 12:24:10 +0530

    set up backend with TypeScript and dev scripts

---
+++ backend/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// A simple console message to confirm TypeScript compiles and runs correctly
+console.log("Backend project is set up correctly!");
+//# sourceMappingURL=index.js.map
---
+++ backend/src/index.ts
@@ -0,0 +1,2 @@
+// A simple console message to confirm TypeScript compiles and runs correctly
+console.log("Backend project is set up correctly!");
